c6ce58434e161e2e54d9112616959c82861a0d3d
Zenpock <>
2025-03-28 16:50:03 -0500

    Added new mapedits option and changed the tower of tragedy option to skip the quiz room entirely, fixed the WW cave of horrors crash

--- TooieRando/OptionData.h
+++ TooieRando/OptionData.h
@@ -6,16 +6,62 @@
 class OptionData
 {
 public:
+	/// <summary>
+	/// This is the type of option this refers to e.g. Flags, Multiselect, Value
+	/// Used to determine how to make the option work like setting flags to true
+	/// </summary>
 	CString OptionType = "";
+
+	/// <summary>
+	/// What name will appear in the UI to indicate what this option does
+	/// </summary>
 	CString optionName;
-	CString defaultValue = ""; //Set by the options list
-	CString currentValue = ""; //Set by the User
-	CString scriptOffset = ""; //The offset within a script to place the edit
-	CString scriptAddress = ""; //The script's index to actually find the associated script to edit
+
+	/// <summary>
+	/// The value used when the option is toggled off
+	/// </summary>
+	CString defaultValue = "";
+
+	/// <summary>
+	/// The value used when the option is toggled on
+	/// </summary>
+	CString currentValue = "";
+
+	/// <summary>
+	/// The offset within the file to the location of the edit
+	/// </summary>
+	CString optionFileOffset = ""; //The offset within a script/map to place the edit
+	
+	/// <summary>
+	/// Used for the offset from the start of the script table/or the Map ID for mapedits
+	/// </summary>
+	CString optionFileIndex = ""; //The script/map's index to actually find the associated script/map to edit
+
+	/// <summary>
+	/// The list of all of the selections when using a multiselect type option
+	/// </summary>
 	std::vector<CString> possibleSelections; //For Enum or multiselect options determines the list of selections in a list
+	
+	/// <summary>
+	/// Whether the functionality for this option will occur
+	/// </summary>
 	bool active = false;
+
+	/// <summary>
+	/// All Flags that will be set when the option is enabled
+	/// </summary>
 	std::vector<int> flags;
+
+	/// <summary>
+	/// Custom script that will be executed in the gcgame
+	/// </summary>
 	CString customCommands = "";
+
+	/// <summary>
+	/// Whether the option will be visible in the ui
+	/// </summary>
+	bool hidden = false;
+
 	/// <summary>
 	/// Id used to reference special options within the randomizer not running in the rom
 	/// </summary>
